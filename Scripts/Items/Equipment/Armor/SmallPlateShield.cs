81256c6799831a847266f48012d3152701176768
Andrew Kirk <scrubtasticx@gmail.com>
2020-06-09 13:51:23 -0400

    Initial Upload

---
+++ Scripts/Items/Equipment/Armor/SmallPlateShield.cs
@@ -0,0 +1,40 @@
+namespace Server.Items
+{
+    [FlipableAttribute(0x4202, 0x420A)]
+    public class SmallPlateShield : BaseShield
+    {
+        [Constructable]
+        public SmallPlateShield()
+            : base(0x4202)
+        {
+            Weight = 6.0;
+        }
+
+        public SmallPlateShield(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int BasePhysicalResistance => 0;
+        public override int BaseFireResistance => 0;
+        public override int BaseColdResistance => 1;
+        public override int BasePoisonResistance => 0;
+        public override int BaseEnergyResistance => 0;
+        public override int InitMinHits => 25;
+        public override int InitMaxHits => 30;
+        public override int StrReq => 35;
+        public override bool CanBeWornByGargoyles => true;
+        public override Race RequiredRace => Race.Gargoyle;
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write(0);//version
+        }
+    }
+}
---
+++ Scripts/Items/Equipment/Weapons/GlassSword.cs
@@ -0,0 +1,45 @@
+namespace Server.Items
+{
+    [FlipableAttribute(0x90C, 0x4073)]
+    public class GlassSword : BaseSword
+    {
+        [Constructable]
+        public GlassSword()
+            : base(0x90C)
+        {
+            Weight = 6.0;
+        }
+
+        public GlassSword(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override WeaponAbility PrimaryAbility => WeaponAbility.BleedAttack;
+        public override WeaponAbility SecondaryAbility => WeaponAbility.MortalStrike;
+        public override int StrengthReq => 20;
+        public override int MinDamage => 11;
+        public override int MaxDamage => 15;
+        public override float Speed => 2.75f;
+
+        public override int DefHitSound => 0x23B;
+        public override int DefMissSound => 0x23A;
+        public override int InitMinHits => 31;
+        public override int InitMaxHits => 90;
+
+        public override Race RequiredRace => Race.Gargoyle;
+        public override bool CanBeWornByGargoyles => true;
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write(0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+        }
+    }
+}
